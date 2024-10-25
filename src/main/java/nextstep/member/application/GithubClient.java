d471c1a36e511bc1411d4d5c3028aa9814f3d48f
mincheolkk <kmc04080408@gmail.com>
2024-10-26 02:26:54 +0900

    feat : 깃헙에 프로필을 요청하는 메서드를 구현한다

--- src/main/java/nextstep/member/application/GithubClient.java
+++ src/main/java/nextstep/member/application/GithubClient.java
@@ -4,9 +4,11 @@ import nextstep.global.exception.AuthenticationException;
 import nextstep.global.exception.GithubConnectionFailException;
 import nextstep.member.application.dto.GithubAccessTokenRequest;
 import nextstep.member.application.dto.GithubAccessTokenResponse;
+import nextstep.member.application.dto.GithubProfileResponse;
 import nextstep.member.config.GithubProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -17,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 public class GithubClient {
 
     private static final String ACCESS_TOKEN_URL = "/github/login/oauth/access_token";
+    private static final String PROFILE_URL = "/github/user";
 
     private final GithubProperties githubProperties;
     private final RestTemplate restTemplate;
@@ -47,6 +50,19 @@ public class GithubClient {
         validateGithubResponse(response);
         validateToken(response.getAccessToken());
         return response.getAccessToken();
+    }
+
+    public GithubProfileResponse requestUserProfile(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " +accessToken);
+
+        var httpEntity = new HttpEntity<>(null, headers);
+        return restTemplate.exchange(
+                PROFILE_URL,
+                HttpMethod.GET,
+                httpEntity,
+                GithubProfileResponse.class
+        ).getBody();
     }
 
     private void validateGithubResponse(GithubAccessTokenResponse response) {
